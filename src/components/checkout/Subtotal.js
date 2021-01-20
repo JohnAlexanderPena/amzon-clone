3bb3d6bafc1330b2aff1a9c5d6a472d79d552d9d
jpena91@gmail.com <jpena91@gmail.com>
2021-01-20 09:05:48 -0500

    add subtotal input

--- src/components/checkout/Subtotal.js
+++ src/components/checkout/Subtotal.js
@@ -5,24 +5,33 @@ import "./Checkout.css";
 const Subtotal = () => {
   return (
     <div className="subtotal">
-      {/* <CurrencyFormat
-        renderText={(value) => (
-          <>
-            <p>
-              Subtotal (0 items): <strong>0</strong>
-            </p>
-            <small className="subtotal__gift">
-              <input type="checkbox" />
-              This order contains a gift
-            </small>
-          </>
-        )}
+      <>
+        <p>
+          Subtotal (0 items): <strong>0</strong>
+        </p>
+        <small className="subtotal__gift">
+          <input type="checkbox" />
+          This order contains a gift
+        </small>
+      </>
+      <CurrencyFormat
+        // renderText={
+        //   <>
+        //     <p>
+        //       Subtotal (0 items): <strong>0</strong>
+        //     </p>
+        //     <small className="subtotal__gift">
+        //       <input type="checkbox" />
+        //       This order contains a gift
+        //     </small>
+        //   </>
+        // }
         decimalScale={2}
         value={0}
         displayText={"text"}
         thousandSeparator={true}
         prefix={"$"}
-      /> */}
+      />
     </div>
   );
 };
