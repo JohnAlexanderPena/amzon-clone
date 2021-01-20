a61147dc891c7a11f94abe247d62a10b4160be78
jpena91@gmail.com <jpena91@gmail.com>
2021-01-20 02:18:00 -0500

    add react-router-dom and switch routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
 import Header from "./modules/header/Header";
 import Home from "./components/home/HomePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./components/checkout/Checkout";
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-      {/* Header */}
-      {/* Home */}
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
