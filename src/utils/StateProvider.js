1cf12eda8fcdb5236783e6dfebef1085b763d5fd
jpena91@gmail.com <jpena91@gmail.com>
2021-01-21 22:52:49 -0500

    setup redux/context api

---
+++ src/utils/StateProvider.js
@@ -0,0 +1,14 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+//Preparationg of the data
+export const StateContext = createContext();
+
+//Wrap our app and provide the data layer
+export const StateProvider = ({ reducer, initialState, children }) => {
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>;
+};
+
+//eporting to access data from data layer
+export const useStateValue = () => useContext(StateContext);
