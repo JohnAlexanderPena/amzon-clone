42746f765dd2d0c90a57491577a7f6bc831d4bff
jpena91@gmail.com <jpena91@gmail.com>
2021-01-21 23:26:49 -0500

    create total function

--- src/components/checkout/Subtotal.js
+++ src/components/checkout/Subtotal.js
@@ -1,14 +1,22 @@
 import React from "react";
 import CurrencyFormat from "react-currency-format";
 import "./Checkout.css";
+import { useStateValue } from "../../utils/StateProvider";
 
 const Subtotal = () => {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       <div>
         <>
           <p>
-            Subtotal (0 items): <strong>0</strong>
+            Subtotal (0 items):{" "}
+            <strong>
+              {basket.reduce(function (tot, arr) {
+                return tot + arr.price;
+              }, 0)}
+            </strong>
           </p>
           <small className="subtotal__gift">
             <input type="checkbox" />
