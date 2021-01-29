1c32edc55759ef2fdfaf03d798d2e96de0f7e807
jpena91@gmail.com <jpena91@gmail.com>
2021-01-28 23:52:00 -0500

    add payment sub component

--- src/components/checkout/payment/Payment.js
+++ src/components/checkout/payment/Payment.js
@@ -1,10 +1,45 @@
 import React from "react";
+import { useStateValue } from "../../../utils/StateProvider";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
 import "./Payment.css";
 
 const Payment = () => {
+  const [{ basket, user }, dispatch] = useStateValue();
   return (
     <div className="payment">
-      <div className="payment__container">Payment Goes Here</div>
+      <div className="payment__container">
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 ABC Lane</p>
+            <p>New York, New York, 10001</p>
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                item={item.id}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+                image={item.image}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title"></div>
+        </div>
+      </div>
     </div>
   );
 };
