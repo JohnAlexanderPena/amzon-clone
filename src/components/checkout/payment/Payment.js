21da23e79f73b0082beac18f69e1d8a36e9a39ae
jpena91@gmail.com <jpena91@gmail.com>
2021-01-29 01:11:40 -0500

    add backend-cloud

--- src/components/checkout/payment/Payment.js
+++ src/components/checkout/payment/Payment.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useStateValue } from "../../../utils/StateProvider";
 import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./Payment.css";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import { getBasketTotal } from "../../../redux/reducer";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
 
 const Payment = () => {
   const [{ basket, user }] = useStateValue();
